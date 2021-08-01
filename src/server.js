1de1754a95ec41e04bd24b7a52e458e426f95f82
Pulan <ekgus5095@gmail.com>
2021-08-01 21:20:56 +0900

    Mongoose Connetion

--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config/key";
 
 const PORT = 4000;
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello world!</h1>"));
+const users = [];
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+const server = async () => {
+  try {
+    await mongoose.connect(config.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+
+    app.use(express.json());
+
+    app.get("/user", function (req, res) {
+      return res.send({ users: users });
+    });
+
+    app.post("/user", function (req, res) {
+      users.push({ name: req.body.name, age: req.body.age });
+      return res.send({ success: true });
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+server();
