8594b44523c0d8087d1a3b970f776d864a7b0291
Pulan <ekgus5095@gmail.com>
2021-08-01 22:42:57 +0900

    Create User Document

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config/key";
+import { User } from "./models/User";
 
 const PORT = 4000;
 const app = express();
-
-const users = [];
 
 const server = async () => {
   try {
@@ -17,13 +16,26 @@ const server = async () => {
 
     app.use(express.json());
 
-    app.get("/user", function (req, res) {
-      return res.send({ users: users });
+    app.get("/user", (req, res) => {
+      // return res.send({ users: users });
     });
 
-    app.post("/user", function (req, res) {
-      users.push({ name: req.body.name, age: req.body.age });
-      return res.send({ success: true });
+    app.post("/user", async (req, res) => {
+      try {
+        let { username, name } = req.body;
+        if (!username)
+          return res.status(400).send({ err: "username is required" });
+        if (!name || !name.first || !name.last)
+          return res
+            .status(400)
+            .send({ err: "Both first and last names are required" });
+        const user = new User(req.body);
+        await user.save();
+        return res.send({ user });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err: err.message });
+      }
     });
 
     app.listen(PORT, () => {
