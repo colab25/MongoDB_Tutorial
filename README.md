ed12747d19546a8e05904190ff5d32a743de8478
Pulan <ekgus5095@gmail.com>
2021-08-04 00:21:29 +0900

    Add user 'Get, Delete, Put' API

---
+++ README.md
@@ -0,0 +1,1 @@
+### branch 'main' set up to track remote branch 'main' from 'origin'. everything up-to-date 에러 확인
--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     name: {
       first: { type: String, required: true },
       last: { type: String, required: true },
