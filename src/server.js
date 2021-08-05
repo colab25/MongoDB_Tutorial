f4b4d7f99a8b0a68c53dab1a425d35bb5e8d3c75
Pulan <ekgus5095@gmail.com>
2021-08-05 22:54:54 +0900

    Debug mode setup and router refactoring

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config/key";
-import { User } from "./models/User";
+import { userRouter } from "./routes/userRoute";
 
 const PORT = 4000;
 const app = express();
@@ -15,82 +15,9 @@ const server = async () => {
       useFindAndModify: false,
     });
     console.log("MongoDB Connected");
-
     app.use(express.json());
 
-    app.get("/user", async (req, res) => {
-      try {
-        const users = await User.find({});
-        return res.send({ users });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-      }
-    });
-
-    app.get("/user/:userId", async (req, res) => {
-      try {
-        const { userId } = req.params;
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: "invalid userId" });
-        const user = await User.findOne({ _id: userId });
-        return res.send({ user });
-      } catch (err) {
-        return res.status(500).send({ err: err.message });
-      }
-    });
-
-    app.post("/user", async (req, res) => {
-      try {
-        let { username, name } = req.body;
-        if (!username)
-          return res.status(400).send({ err: "username is required" });
-        if (!name || !name.first || !name.last)
-          return res
-            .status(400)
-            .send({ err: "Both first and last names are required" });
-        const user = new User(req.body);
-        await user.save();
-        return res.send({ user });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-      }
-    });
-
-    app.delete("/user/:userId", async (req, res) => {
-      try {
-        const { userId } = req.params;
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: "invalid userId" });
-        const user = await User.findOneAndDelete({ _id: userId });
-        return res.send({ user });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-      }
-    });
-
-    app.put("/user/:userId", async (req, res) => {
-      try {
-        const { userId } = req.params;
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: "invalid userId" });
-        const { age } = req.body;
-        if (!age) return res.status(400).send({ err: "age is required" });
-        if (typeof age !== "number")
-          return res.status(400).send({ err: "age must be a number" });
-        const user = await User.findByIdAndUpdate(
-          userId,
-          { $set: { age } },
-          { new: true }
-        );
-        return res.send({ user });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-      }
-    });
+    app.use("/user", userRouter);
 
     app.listen(PORT, () => {
       console.log(`Server listening on port http://localhost:${PORT}`);
